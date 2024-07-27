032c5cf2d655b86ec0d77dc24cf52eb7ab270356
firasami <154001918+firasami@users.noreply.github.com>
2024-07-27 22:05:51 +0300

    Add files via upload

--- pythonic.py
+++ pythonic.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 
 app = Flask(__name__)
+
+app.config[
+    "SECRET_KEY"
+] = "518b27555b2c665ab650263a6f5f7ef46f3671072acb21c14924bd222c9b0236"
+
 
 News = [{
     'title': 'news 1',
@@ -61,6 +68,31 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title="About")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created successfully for {form.username.data}", "success")
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if (
+            form.email.data == "omar@email.com"
+            and form.password.data == "PASS!!word123"
+        ):
+            flash("You have been logged in!", "success")
+            return redirect(url_for("home"))
+        else:
+            flash("Login Unsuccessful. Please check credentials", "danger")
+    return render_template("login.html", title="Login", form=form)
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
